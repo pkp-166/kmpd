4cf96e5e0c7002c1bb35ceec3e8caf6108d2f86e
pkp-166 <123925482+pkp-166@users.noreply.github.com>
2023-08-25 15:13:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,56 @@ import wikipedia
 import datetime
 import openai
 
-openai.api_key='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'
+openai.api_key='import pyjokes
+import pywhatkit
+import wikipedia
+import datetime
+import openai
+
+openai.api_key='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'
+
+if __name__ == "__main__":
+    print("Hello, Assistant here")
+    while True:
+            messages = []
+            system_msg = 'sharp and formal'
+            messages.append({"role": "system", "content": system_msg})
+            print("Assistant is ready!.. press \'quit()\' to stop chatting.. ")
+            command = input()
+            messages.append({"role": "user", "content": command})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages)
+            reply = response["choices"][0]["message"]["content"]
+            messages.append({"role": "assistant", "content": reply})
+            if("I'm sorry" not in reply):
+                print("\n" + reply + "\n")
+            else:
+                continue
+            command=command.lower()
+            if 'play' in command:
+                song = command.replace('play','')
+                pywhatkit.playonyt(song)
+            elif (('search' or 'wiki' or 'wikipedia') in command):
+                print(pywhatkit.info(command,lines=15))
+                command2=input("\nDo you want to see the search results in google? ")
+                if command2.lower()=='yes':
+                    print(pywhatkit.search(command),"\n")
+                else:
+                    break
+            elif 'time' in command:
+                time = datetime.datetime.now().strftime('%I:%M %p')
+                print('Current time is ' + time)
+            elif 'who the heck is' in command:
+                person = command.replace('who the heck is', '')
+                info = wikipedia.summary(person, 1)
+                print(info)
+            elif (('joke' or 'jokes') in command):
+                while(command!="no"):
+                    print(pyjokes.get_joke())
+                    command=input("Another one? ")
+                    command=command.lower()
+
 
 if __name__ == "__main__":
     print("Hello, Assistant here")
